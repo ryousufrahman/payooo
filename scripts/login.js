441654564f8eb3220cebc4efc59c23e3ba95f7b7
yousufrahman <ryousuf766@gmail.com>
2026-02-17 23:37:58 +0600

    made login page functional

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,17 @@
-console.log('script connected');
+document.getElementById('log-in-btn').addEventListener('click', function(){
+    const contactNumber = document.getElementById('contact-number')
+    const validNumber = contactNumber.value;
+     const logInPin = document.getElementById('login-pin');
+     const validPin = logInPin.value;
+
+     if(validNumber=="01234567890" && validPin== '1234'){
+       
+        window.location.assign("./home.html")
+
+     }
+     else(
+        alert('Invalid Information')
+        
+     )
+    
+})
