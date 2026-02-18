b8de79146dd2a16b80173e49df161779eb989880
yousufrahman <ryousuf766@gmail.com>
2026-02-18 23:51:49 +0600

    almost done building the logic left just password logic

---
+++ scripts/cashout.js
@@ -0,0 +1,31 @@
+document.getElementById('cashOutButton').addEventListener('click', function(){
+    const agentElement = document.getElementById('agentNumber').value ;
+    if(agentElement.length!=11){
+        alert('invalid agent number')
+        return;
+    }
+
+    const balenceElement = document.getElementById('balance').innerText;
+    const amountInput = document.getElementById('amount-cashOut').value;
+    const balenceInNumber = Number(balenceElement);
+
+    if(balenceInNumber - amountInput < 0){
+        alert('insufficient')
+        return;
+    }
+    
+    const pinNumber = document.getElementById('pinNumber').value;
+    const balenceTag = document.getElementById('balance');
+
+    if(pinNumber ==='1234'){
+        alert('Cash-Out Successful')
+        const newBalence = balenceInNumber - amountInput;
+        balenceTag.innerText = newBalence;
+         
+    }
+    else{
+        alert('Wrong password')
+        
+    }
+    
+})
