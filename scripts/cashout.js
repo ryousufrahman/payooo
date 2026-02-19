bae5aa3a89b9ea18f560e8fd7d9fd8b23b2a981a
yousufrahman <ryousuf766@gmail.com>
2026-02-19 12:29:49 +0600

    added a machine to get the value from input feild and applied the machine to code commenting the orginal code

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -1,31 +1,67 @@
+// using machin
 document.getElementById('cashOutButton').addEventListener('click', function(){
-    const agentElement = document.getElementById('agentNumber').value ;
-    if(agentElement.length!=11){
+
+    const agentNumber = getValueFromInput('agentNumber');
+    if(agentNumber.length !=11){
         alert('invalid agent number')
         return;
     }
-
-    const balenceElement = document.getElementById('balance').innerText;
-    const amountInput = document.getElementById('amount-cashOut').value;
-    const balenceInNumber = Number(balenceElement);
-
-    if(balenceInNumber - amountInput < 0){
-        alert('insufficient')
+    const balenceAmount = document.getElementById('balance').innerText;
+    const amountInput = getValueFromInput('amount-cashOut');
+    const balenceInNumber = Number(balenceAmount);
+   if(balenceInNumber - amountInput < 0){
+        alert('insufficient');
         return;
     }
-    
-    const pinNumber = document.getElementById('login-pin').value;
-    const balenceTag = document.getElementById('balance');
 
-    if(pinNumber == '1234'){
-        alert('Cash-Out Successful')
-        const newBalence = balenceInNumber - amountInput;
-        balenceTag.innerText = newBalence;
-         
-    }
-    else{
-        alert('Wrong password')
-        
-    }
+  const pin = getValueFromInput('login-pin');
+  const balenceElement =document.getElementById('balance')
+   if(pin ==='1234'){
+     alert('Cash-Out Successful')
+     const newBalence = balenceInNumber - amountInput;
+     balenceElement.innerText = newBalence;
+
+   }
+   else{
+    alert('invaid pin')
+   }
     
 })
+
+
+
+
+
+
+
+// document.getElementById('cashOutButton').addEventListener('click', function(){
+//     const agentElement = document.getElementById('agentNumber').value ;
+//     if(agentElement.length!=11){
+//         alert('invalid agent number')
+//         return;
+//     }
+
+//     const balenceElement = document.getElementById('balance').innerText;
+//     const amountInput = document.getElementById('amount-cashOut').value;
+//     const balenceInNumber = Number(balenceElement);
+
+//     if(balenceInNumber - amountInput < 0){
+//         alert('insufficient')
+//         return;
+//     }
+    
+//     const pinNumber = document.getElementById('login-pin').value;
+//     const balenceTag = document.getElementById('balance');
+
+//     if(pinNumber == '1234'){
+//         alert('Cash-Out Successful')
+//         const newBalence = balenceInNumber - amountInput;
+//         balenceTag.innerText = newBalence;
+         
+//     }
+//     else{
+//         alert('Wrong password')
+        
+//     }
+    
+// })
---
+++ scripts/machin.js
@@ -0,0 +1,6 @@
+function getValueFromInput(id) {
+  const theInputElement = document.getElementById(id);
+  const theInputValue = theInputElement.value;
+ return theInputValue;
+    
+}
